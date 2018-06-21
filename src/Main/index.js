b344833dfa9b793daf1a71a1700e21e234968d0c
omelenetskiy <amelianetski@gmail.com>
2018-06-21 08:37:32 +0300

    add img to jest configs

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Main.sass';
+import logo from '../static/img/react/logo.png';
 
 class Main extends Component {
 	state = {};
@@ -7,6 +8,7 @@ class Main extends Component {
 		return (
 			<div>
 				<h2>Main</h2>
+				<img src={logo} alt="" />
 			</div>
 		);
 	}
